47b7ac1fec6993dd8e10700de4aa36e8f1659d7b
Joyonto Karmakar <41202398+Joyontokarmakar@users.noreply.github.com>
2021-02-03 19:44:04 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,13 @@
+let bonus = 30;
+function sum (first, second){
+    const result = first + second;
+    // console.log(bonus);
+    return result;
+
+}
+// console.log(day);
+let day = "Tuesday";
+console.log(day);
+
+const ans = sum(10, 30);
+// console.log(ans);
