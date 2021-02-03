d4de838ae9a6921c4bdc9fd58f8983cfb7d1e986
Joyonto Karmakar <41202398+Joyontokarmakar@users.noreply.github.com>
2021-02-03 20:23:23 +0600

    slice, splice, join

---
+++ array_split.js
@@ -0,0 +1,27 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// slice:
+//which index to which index
+// const part1 = numbers.slice(2, 5);
+// console.log(part1);
+// console.log(numbers);
+
+// splice:
+// from which index  and how many
+// const remove = numbers.splice(2,3);
+// console.log(remove);
+// console.log(numbers);
+
+// splice and add:
+// add some element in where splice start. 
+// const remove = numbers.splice(2, 3, 10, 11, 12, 14);
+// console.log(remove);
+// console.log(numbers);
+
+// join:
+// elements are join by "how you want?" 
+// const together = numbers.join(",");
+// const together = numbers.join(" ");
+// const together = numbers.join(" ami ");
+console.log(together);
+console.log(numbers);
